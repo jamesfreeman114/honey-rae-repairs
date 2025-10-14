9eb5b1e0423ce7d18603f92a0cb372936538db42
James Freeman <jamesfreeman114@gmail.com>
2025-10-13 20:47:07 -0500

    Implemented useState React Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import react from "react"
+import { useState } from "react"
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+  
+  const [count, setCount] = useState(0) //[stateVariable, setterFunction]
+
+  const handleButtonClick = () => {
+  setCount(count + 1)
+}
+
+  return <>
+  <h2>Hello</h2>
+  <div>This is amazing!</div>
+  <button className="btn-secondary" onClick={handleButtonClick}>Click Me!</button>
+  <div>Count: {count}</div>
+  </>
 }
